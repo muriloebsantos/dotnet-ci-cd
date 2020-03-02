45e6ae1e8567727ebbd4bc6b855407e20f2b4259
Murilo Santos <murilo.bacagini@gmail.com>
2020-03-02 20:22:31 -0300

    change

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace dotnet_ci_cd.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "value3" };
         }
 
         // GET api/values/5
